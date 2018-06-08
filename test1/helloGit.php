40f29c83a9d9c59b01fbcbe7c82719f1206ab882
ransom <ransomcailing7@gmail.com>
2018-06-08 16:56:10 +0800

    loop add

--- test1/helloGit.php
+++ test1/helloGit.php
@@ -4,4 +4,13 @@
 	echo "Welcome to my first program in uploading to git repository.";
 	echo "hahahahahahah...gwapo kaayo ko ..hahahahah";
 	echo "for my love will see you through.";
+ 
+
+
+
+for ($i=0; $i < ; $i++) { 
+
+	echo $i;
+
+}
  ?>
